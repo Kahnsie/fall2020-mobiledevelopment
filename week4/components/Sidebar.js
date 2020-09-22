a580f18a13aa06fb509a3e017cc2ef66d489f507
Kahnsie <70824560+Kahnsie@users.noreply.github.com>
2020-09-22 19:56:00 -0400

    Add files via upload

--- week4/components/Sidebar.js
+++ week4/components/Sidebar.js
@@ -11,7 +11,8 @@ class Sidebar extends Component {
     let responseHTML = '';
 
     this.props.menu.forEach( (item) => {
-      // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
+      
+      <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
     });
 
     return responseHTML;
