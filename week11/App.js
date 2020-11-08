7b1e912ef7ddfecc5cf4cafc9cb6b31caff52c32
Kahnsie <kahncarley@gmail.com>
2020-11-07 21:52:11 -0500

    week11
    Put my two favorite foods, submit button, and name

---
+++ week11/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Card> Kahn Carley </Card>
+      <Text>I love Chinese Food</Text>
+      <Text>I love Ice Cream</Text>
+      <StatusBar style="auto" />
+      <Button> SUBMIT </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
